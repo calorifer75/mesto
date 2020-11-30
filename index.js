322ada9e1c39904fa2761c42591087b41edaa665
Viacheslav Voytenko <calorifer75@yandex.ru>
2020-11-30 16:46:09 +0300

    fix: Замечания первого ревью

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ let profileSubtitle = document.querySelector(".profile__subtitle");
 
 let popup = document.querySelector(".popup");
 let popupCloseButton = document.querySelector(".popup__close-button");
-let popupName = document.querySelector("#name");
-let popupAbout = document.querySelector("#about");
+let popupName = document.querySelector(".popup__input_name");
+let popupAbout = document.querySelector(".popup__input_about");
 
-let form = document.querySelector("#popup-form");
+let form = document.querySelector(".popup__form");
 
-profileEditButton.addEventListener("click", function () {
+function popupOpen() {
   popup.classList.add("popup_opened");
   popupName.value = profileTitle.textContent;
   popupAbout.value = profileSubtitle.textContent;
-});
+}
 
-popupCloseButton.addEventListener("click", function () {
+function popupClose() {
   popup.classList.remove("popup_opened");
-});
+}
 
 function formSubmitHandler(event) {
   event.preventDefault();
@@ -25,7 +25,9 @@ function formSubmitHandler(event) {
   profileTitle.textContent = popupName.value;
   profileSubtitle.textContent = popupAbout.value;
 
-  popup.classList.remove("popup_opened");
+  popupClose();
 }
 
+profileEditButton.addEventListener("click", popupOpen);
+popupCloseButton.addEventListener("click", popupClose);
 form.addEventListener("submit", formSubmitHandler);
