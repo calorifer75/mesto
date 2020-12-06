5ff5642c571a87eb3279ece7fa5754927852b07e
Viacheslav Voytenko <calorifer75@yandex.ru>
2020-12-06 12:39:29 +0300

    feat: рендер карточек через JS

--- index.js
+++ index.js
@@ -9,6 +9,33 @@ let popupAbout = document.querySelector(".popup__input_type_about");
 
 let form = document.querySelector(".popup__form");
 
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 function popupOpen() {
   popup.classList.add("popup_opened");
   popupName.value = profileTitle.textContent;
@@ -27,6 +54,18 @@ function formSubmitHandler(event) {
 
   popupClose();
 }
+
+function makeCard(item) {
+  let cardTemplateElement = document.querySelector('.cards__template').content;
+  let newCardElement = cardTemplateElement.cloneNode(true);
+  newCardElement.querySelector('.cards__photo').src = item.link;
+  newCardElement.querySelector('.cards__title').textContent = item.name;
+  return newCardElement;
+}
+
+
+const cards = initialCards.map(makeCard);
+document.querySelector('.cards').append(...cards);
 
 profileEditButton.addEventListener("click", popupOpen);
 popupCloseButton.addEventListener("click", popupClose);
